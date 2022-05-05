5492a4fbb2c225d2380e8785983f826c1877b7f0
Michal <michalnoga.poczta@gmail.com>
2022-05-05 21:48:42 +0200

    MATURA 2021 - zadanie 3.2

---
+++ 2021/zadanie_3.2/main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    print(int('10000000', 2) > int('A9', 16))
+    print(int('1111', 4) > int('1111111', 2))
+    print(int('3003', 4) > int('C2', 16))
+    print(int('333', 8) > int('10100101', 2))
