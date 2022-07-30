4abbfe644e387eec1a1b010874d69129634fcfe9
Michal <michalnoga.poczta@gmail.com>
2022-07-30 21:47:02 +0200

    MATURA 2021 - zadanie 1.1

--- 2021/C/zadanie_1.1/Task_11.cpp
+++ 2021/C/zadanie_1.1/Task_11.cpp
@@ -1,5 +1,25 @@
-//
-// Created by Michal on 05.09.2022.
-//
+#include <valarray>
+#include <iostream>
 
-#include "Task_11.h"
+using namespace std;
+
+int findBiggestDifference(int min, int max) {
+    int output = 0;
+
+    for (int i = min; i < max; i++) {
+        int difference = abs(max - i);
+        if (difference > output) {
+            output = difference;
+        }
+    }
+    return output;
+}
+
+int main() {
+    setlocale(LC_CTYPE, "Polish");
+
+    int biggestDifference = findBiggestDifference(1000, 9999);
+    std::cout << "Największa różnica wynosi: " << biggestDifference << std::endl;
+
+    return 0;
+}
