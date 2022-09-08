6627b6370371e8b7c36eb23a08144d57b7fb8274
Michal <michalnoga.poczta@gmail.com>
2022-09-08 21:15:05 +0200

    MATURA 2021 - zadanie 4.2

--- 2021/C/zadanie_4.2/Task_42.cpp
+++ 2021/C/zadanie_4.2/Task_42.cpp
@@ -1,5 +1,46 @@
-//
-// Created by Michal on 20.09.2022.
-//
+#include <string>
+#include <fstream>
 
-#include "Task_42.h"
+using namespace std;
+
+int main() {
+    string command;
+    char argument;
+    int longestSequence = 0;
+    string longestSequenceInstruction;
+    int longestSequenceTemp = 0;
+    string longestSequenceInstructionTemp;
+    string previousCommand;
+
+    fstream inputFile(
+            R"(C:\Users\Michal\Documents\instrukcje.txt)",
+            fstream::in);
+    if (!inputFile) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    while (inputFile >> command >> argument) {
+        if (command == previousCommand) {
+            longestSequenceTemp++;
+            longestSequenceInstructionTemp = command;
+        } else {
+            if (longestSequenceTemp > longestSequence) {
+                longestSequence = longestSequenceTemp;
+                longestSequenceInstruction = longestSequenceInstructionTemp;
+            }
+            longestSequenceTemp = 1;
+            longestSequenceInstructionTemp = "";
+        }
+        previousCommand = command;
+    }
+
+    inputFile.close();
+
+    ofstream output;
+    output.open(R"(C:\Users\Michal\Documents\wynik4.txt)");
+    output << longestSequence << " - " << longestSequenceInstruction << endl;
+    output.close();
+
+    return 0;
+}
