b4862becfd1f169b4275a88c87d6d93a3a0a17c7
Michal <michalnoga.poczta@gmail.com>
2022-09-08 20:12:54 +0200

    MATURA 2021 - zadanie 4.2

---
+++ 2021/C/zadanie_4.2/Task_42.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michal on 20.09.2022.
+//
+
+#include "Task_42.h"
---
+++ 2021/C/zadanie_4.2/Task_42.h
@@ -0,0 +1,14 @@
+//
+// Created by Michal on 20.09.2022.
+//
+
+#ifndef C_TASK_42_H
+#define C_TASK_42_H
+
+
+class Task_42 {
+
+};
+
+
+#endif //C_TASK_42_H
